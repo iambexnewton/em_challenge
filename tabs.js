ac3e739369058de06eb563a6daceea2388d3688a
iambexnewton <iambexnewton@gmail.com>
2025-03-19 11:59:14 +0100

    em/initial render of data no styles

--- tabs.js
+++ tabs.js
@@ -1,1 +1,38 @@
-console.log('log from tabs.js');
+
+export const tabsData = [
+    {
+      id: 1,
+      title: "First Tab",
+      content: {
+        heading: "First Heading",
+        body: "First body",
+      },
+    },
+    {
+      id: 2,
+      title: "Second Tab",
+      content: {
+        heading: "Second Heading",
+        body: "Second body",
+      },
+    },
+    {
+      id: 3,
+      title: "Third Tab",
+      content: {
+        heading: "Third Heading",
+        body: "Third body",
+      },
+    },
+  ];
+
+  export function renderData(){
+    const appContainer = document.querySelector("#app")
+    const renderData = tabsData.map((tab, index) => 
+      `<div id=${index}>
+       <h2>${tab.content.heading}</h2>
+       </div>`  
+   )
+    console.log(renderData)
+    appContainer.innerHTML = `<div> ${renderData}</div>`
+  }
