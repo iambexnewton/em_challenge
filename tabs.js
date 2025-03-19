009b498fc6cb0fd575fd806b55aab0729b48317c
iambexnewton <iambexnewton@gmail.com>
2025-03-19 12:24:09 +0100

    em/styling with first tab index 0 open, todo functionality

--- tabs.js
+++ tabs.js
@@ -1,59 +1,53 @@
 export const tabsData = [
-  {
-    id: 1,
-    title: "First Tab",
-    content: {
-      heading: "First Heading",
-      body: "First body",
+    {
+        id: 1,
+        title: "First Tab",
+        content: {
+            heading: "First Heading",
+            body: "First body",
+        },
     },
-  },
-  {
-    id: 2,
-    title: "Second Tab",
-    content: {
-      heading: "Second Heading",
-      body: "Second body",
+    {
+        id: 2,
+        title: "Second Tab",
+        content: {
+            heading: "Second Heading",
+            body: "Second body",
+        },
     },
-  },
-  {
-    id: 3,
-    title: "Third Tab",
-    content: {
-      heading: "Third Heading",
-      body: "Third body",
+    {
+        id: 3,
+        title: "Third Tab",
+        content: {
+            heading: "Third Heading",
+            body: "Third body",
+        },
     },
-  },
 ];
 
-//   export function renderData(){
-//     const appContainer = document.querySelector("#app")
-//     const renderData = tabsData.map((tab, index) =>
-//       `<div id=${index}>
-//        <h2>${tab.content.heading}</h2>
-//        </div>`
-//    )
-//     console.log(renderData)
-//     appContainer.innerHTML = `<div> ${renderData}</div>`
-//   }
-
 export function renderTabs() {
-  const tabButtons = tabsData.map(
-    (tab) =>
-      `<button
-        role='tab'
+    const tabButtons = tabsData
+        .map(
+            (tab, index) =>
+                `<button
+        aria-selected="${index === 0 ? 'true' : 'false'}" 
+     role='tab'
      >${tab.title}</button>`
-  );
+        )
+        .join("");
 
-  const tabPanels = tabsData.map(
-    (tab) =>
-      `<div
+    const tabPanels = tabsData
+        .map(
+            (tab) =>
+                `<div
      role='tabpanel'
      >
      <h2>${tab.content.heading}</h2>
      <p>${tab.content.body}</p></div>`
-  );
+        )
+        .join("");
 
-  return `
+    return `
 <div class="container">
   <div class="tabs" role="tablist">
     ${tabButtons}
@@ -68,14 +62,13 @@ export function renderTabs() {
 export function tabFunctionality() {
     const tabs = document.querySelectorAll('[role="tab"]');
 
-    tabs.forEach(tab => {
-        tab.addEventListener('click', e => {
-            console.log("tab clicked", e)
-            console.log("tab clicked", e.currentTarget)
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", (e) => {
+            console.log("tab clicked", e);
+            console.log("tab clicked", e.currentTarget);
             e.preventDefault();
         });
     });
 }
 
-
-document.querySelector('#app').innerHTML = renderTabs(tabsData);
+document.querySelector("#app").innerHTML = renderTabs(tabsData);
