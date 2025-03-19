93e76a0ba1fbc5b0605815b0636dcd206e1d946f
iambexnewton <iambexnewton@gmail.com>
2025-03-19 12:08:32 +0100

    em/tabs and panels rendering data no styling no functionality

--- tabs.js
+++ tabs.js
@@ -1,38 +1,67 @@
-
 export const tabsData = [
-    {
-      id: 1,
-      title: "First Tab",
-      content: {
-        heading: "First Heading",
-        body: "First body",
-      },
+  {
+    id: 1,
+    title: "First Tab",
+    content: {
+      heading: "First Heading",
+      body: "First body",
     },
-    {
-      id: 2,
-      title: "Second Tab",
-      content: {
-        heading: "Second Heading",
-        body: "Second body",
-      },
+  },
+  {
+    id: 2,
+    title: "Second Tab",
+    content: {
+      heading: "Second Heading",
+      body: "Second body",
     },
-    {
-      id: 3,
-      title: "Third Tab",
-      content: {
-        heading: "Third Heading",
-        body: "Third body",
-      },
+  },
+  {
+    id: 3,
+    title: "Third Tab",
+    content: {
+      heading: "Third Heading",
+      body: "Third body",
     },
-  ];
+  },
+];
 
-  export function renderData(){
-    const appContainer = document.querySelector("#app")
-    const renderData = tabsData.map((tab, index) => 
-      `<div id=${index}>
-       <h2>${tab.content.heading}</h2>
-       </div>`  
-   )
-    console.log(renderData)
-    appContainer.innerHTML = `<div> ${renderData}</div>`
-  }
+//   export function renderData(){
+//     const appContainer = document.querySelector("#app")
+//     const renderData = tabsData.map((tab, index) =>
+//       `<div id=${index}>
+//        <h2>${tab.content.heading}</h2>
+//        </div>`
+//    )
+//     console.log(renderData)
+//     appContainer.innerHTML = `<div> ${renderData}</div>`
+//   }
+
+export function renderTabs() {
+  const tabButtons = tabsData.map(
+    (tab) =>
+      `<button
+        role='tab'
+     >${tab.title}</button>`
+  );
+
+  const tabPanels = tabsData.map(
+    (tab) =>
+      `<div
+     role='tabpanel'
+     >
+     <h2>${tab.content.heading}</h2>
+     <p>${tab.content.body}</p></div>`
+  );
+
+  return `
+<div class="container">
+  <div class="tabs" role="tablist">
+    ${tabButtons}
+  </div>
+  <div class="panels">
+    ${tabPanels}
+  </div>
+</div>
+`;
+}
+document.querySelector('#app').innerHTML = renderTabs(tabsData);
